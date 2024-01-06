9b9ce06f545f5fc49f159ad1044871219d8e12e5
Viviane <vivianeaguiarc@outlook.com>
2024-01-06 08:49:40 -0300

    Finalizando projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
-const gulp = require('gulp')
+const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
@@ -7,7 +9,14 @@ function styles(){
     .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
-exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
+
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
